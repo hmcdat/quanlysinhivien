ddabab6f89d580adf8a6d64adc8aa66aeda66a75
Hoàng Mai Công Đạt <admin@hmcdat.dev>
2021-11-27 11:33:05 +0700

    Initial commit

---
+++ src/main/java/com/hmcdat/quanlysinhivien/QuanlysinhivienApplication.java
@@ -0,0 +1,13 @@
+package com.hmcdat.quanlysinhivien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlysinhivienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlysinhivienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hmcdat/quanlysinhivien/QuanlysinhivienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmcdat.quanlysinhivien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlysinhivienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
