75a0134be28021bf49282d9e2f30f9bbf0ab5d0d
Hoàng Mai Công Đạt <admin@hmcdat.dev>
2021-11-30 01:07:15 +0700

    add getter and setter

--- src/main/java/com/hmcdat/quanlysinhivien/models/Department.java
+++ src/main/java/com/hmcdat/quanlysinhivien/models/Department.java
@@ -24,4 +24,52 @@ public class Department implements Serializable {
 
     @Column(name = "address")
     private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public int getViceManagerId() {
+        return viceManagerId;
+    }
+
+    public void setViceManagerId(int viceManagerId) {
+        this.viceManagerId = viceManagerId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
